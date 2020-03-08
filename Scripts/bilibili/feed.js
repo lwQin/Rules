77bf1685893ff3cd762da6244b3e74f3d98024f1
lwQin <justoncesssss@gmail.com>
2020-03-08 22:14:01 +0800

    update

--- Scripts/bilibili/feed.js
+++ Scripts/bilibili/feed.js
@@ -1,9 +1,11 @@
 let body = $response.body
 body = JSON.parse(body)
 body['data']['items'].forEach((element, index) => {
-	if(element.hasOwnProperty('ad_info')) {
+	if (element.hasOwnProperty('ad_info')) {
 		body['data']['items'].splice(index, 1)
 	}
 })
 body = JSON.stringify(body)
-$done({body})
+$done({
+	body
+})

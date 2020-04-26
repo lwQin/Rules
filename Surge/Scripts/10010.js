4caab57a43b2ce504ff3c38d2891f1e14543ab36
lwQin <justoncesssss@gmail.com>
2020-04-26 14:14:06 +0800

    update china unicom adblock module

--- Surge/Scripts/10010.js
+++ Surge/Scripts/10010.js
@@ -4,6 +4,7 @@ if ($request.url.indexOf('accountListData.htm')) {
   body = JSON.parse($response.body)
   if (body.hasOwnProperty('adv')) {
     delete body.adv
+    body = JSON.stringify(body)
   }
 } else {
   body = JSON.stringify({})

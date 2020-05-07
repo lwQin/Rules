a7678826acfb0f9c1653f1e3504c073b5326a5e6
lwQin <justoncesssss@gmail.com>
2020-05-07 21:53:44 +0800

    update bilibili script and module

--- Surge/Scripts/bilibili.js
+++ Surge/Scripts/bilibili.js
@@ -1,56 +1,22 @@
-let body = JSON.parse($response.body)
+//  else if ($request.url.includes('x/v2/space')) {
+//   space(body)
+// } else if ($request.url.includes('x/resource/show/tab')) {
+//   tab(body)
+// } else if ($request.url.includes('x/v2/account/mine')) {
+//   mine(body)
+// } else if ($request.url.includes('x/v2/view')) {
+//   view(body)
+// } else if ($request.url.includes('dynamic_history')) {
+//   dynamicHistory(body)
+// }
 
-if ($request.url.includes('x/v2/feed/index')) {
-  feed(body)
-} else if ($request.url.includes('x/v2/space')) {
-  space(body)
-} else if ($request.url.includes('x/resource/show/tab')) {
-  tab(body)
-} else if ($request.url.includes('x/v2/account/mine')) {
-  mine(body)
-} else if ($request.url.includes('x/v2/view')) {
-  view(body)
-} else if ($request.url.includes('dynamic_history')) {
-  dynamicHistory(body)
-}
-
-body = JSON.stringify(body)
-$done({
-  body
-})
-
-/**
- * 首页 banner 与信息流去广告
- * @param {*} body
- */
-function feed(body) {
-  let newItems = body.data.items.filter((item) => {
-    if (item.hasOwnProperty('banner_item')) {
-      // banner
-      let newBanners = item.banner_item.filter(banner => !(banner.hasOwnProperty('is_ad') || banner.hasOwnProperty('ad_cb')))
-      item.banner_item = newBanners
-    }
-    // 信息流
-    return !item.hasOwnProperty('ad_info')
-  })
-  body.data.items = newItems
-}
-
-/**
- * 去除空间页面推广橱窗
- * @param {*} body
- */
-function space(body) {
-  delete body.data.ad_source_content_v2
-  delete body.data.ad_shop_type
-}
 
 /**
  * 去顶部游戏中心, 自定义顶部 tab
  * @param {*} body
  */
 function tab(body) {
-  const topTabNameWhiteList = ['直播', '推荐', '热门', '追番', '影视', '学习区']
+  const topTabNameWhiteList = ['直播', '推荐', '热门', '追番']
   let topTab = body.data.tab.filter(tab => topTabNameWhiteList.includes(tab.name))
   body.data.tab = topTab
 
@@ -63,18 +29,69 @@ function tab(body) {
 }
 
 /**
- * 去除底部大会员 tab, 去除“我的” tab 中去除多余项
+ * 首页 banner 与信息流去广告
+ * @param {*} body
+ */
+function feed(body) {
+  let newItems = body.data.items.filter((item) => {
+    if (item.banner_item) {
+      // banner
+      let newBanners = item.banner_item.filter(banner => !(banner.is_ad || banner.ad_cb))
+      item.banner_item = newBanners
+    }
+    // 信息流
+    return !item.ad_info
+  })
+  body.data.items = newItems
+}
+
+/**
+ * 去除“我的” tab 中去除多余项
  * @param {*} body
  */
 function mine(body) {
-  let sections = body.data.sections.filter((section) => {
-    const titleWhiteList = ['创作首页', '稿件管理', '离线缓存', '历史记录', '我的收藏', '稍后再看', '我的钱包', '大会员', '我的客服']
-    let items = section.items.filter(item => titleWhiteList.includes(item.title))
-    section.items = items
-
-    return true
+  let sections = body.data.sections_v2.filter((section) => {
+    if (section.title) {
+      const titleWhiteList = ['创作首页', '稿件管理', '个性装扮', '我的钱包', '会员购中心', '联系客服', '设置']
+      let items = section.items.filter(item => titleWhiteList.includes(item.title))
+      section.items = items
+      return true
+    }
   })
-  body.data.sections = sections
+  body.data.sections_v2 = sections
+}
+
+/**
+ * 动态去除热门话题
+ * @param {*} body
+ */
+function hotEntry(body) {
+  body.data = {}
+}
+
+/**
+ * 动态信息流去广告
+ * @param {*} body
+ */
+function dynamic(body) {
+  let cards = []
+  let oldCards = body.data.cards
+  oldCards.forEach(elememt => {
+    let cardDetail = JSON.parse(elememt.card)
+    if (!cardDetail.hasOwnProperty('ad_ctx')) {
+      cards.push(elememt)
+    }
+  });
+  body.data.cards = cards
+}
+
+/**
+ * 去除空间页面推广橱窗
+ * @param {*} body
+ */
+function space(body) {
+  delete body.data.ad_source_content_v2
+  delete body.data.ad_shop_type
 }
 
 /**
@@ -87,18 +104,41 @@ function view(body) {
   body.data.relates = relates
 }
 
-/**
- * 动态信息流去广告
- * @param {*} body
- */
-function dynamicHistory(body) {
-  let cards = []
-  let oldCards = body.data.cards
-  oldCards.forEach(elememt => {
-    let cardDetail = JSON.parse(elememt.card)
-    if (!cardDetail.hasOwnProperty('ad_ctx')) {
-      cards.push(elememt)
-    }
-  });
-  body.data.cards = cards
+
+if ($request.url.indexOf('/resource/show/tab') !== -1) {
+  let body = JSON.parse($response.body)
+  tab(body)
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('/feed/index') !== -1) {
+  let body = JSON.parse($response.body)
+  feed(body)
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('/account/mine') !== -1) {
+  let body = JSON.parse($response.body)
+  mine(body)
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('/topic_svr/hot_entry') !== -1) {
+  let body = JSON.parse($response.body)
+  hotEntry(body)
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
 }
+// else if ($request.url.indexOf('/dynamic_svr/dynamic_new') !== -1) {
+//   let body = JSON.parse($response.body)
+//   dynamic(body)
+//   body = JSON.stringify(body)
+//   $done({
+//     body
+//   })
+// }

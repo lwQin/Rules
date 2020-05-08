5406f8349abdfb4c91d0d11e067a7890beb3b2aa
lwQin <justoncesssss@gmail.com>
2020-05-08 20:24:39 +0800

    update china unicom script and module

--- Surge/Scripts/10010.js
+++ Surge/Scripts/10010.js
@@ -1,32 +1,32 @@
-if ($request.url.indexOf('accountListData.htm') !== -1) {
+if ($request.url.indexOf('accountListData.htm')) {
   let body = JSON.parse($response.body)
   delete body.adv
   body = JSON.stringify(body)
   $done({
     body
   })
-} else if ($request.url.indexOf('getHomePageAd') !== -1) {
-  let body = JSON.stringify({})
-  $done({
-    body
-  })
-} else if ($request.url.indexOf('getDataFromService') !== -1) {
-  if ($request.method === 'POST') {
-    let body = JSON.stringify({})
-    $done({
-      body
-    })
-  }
-} else if ($request.url.indexOf('indexExpressNewsAndRecommend.htm') !== -1) {
+} else if ($request.url.indexOf('indexExpressNewsAndRecommend.htm')) {
   let body = JSON.parse($response.body)
   body.content = []
   body = JSON.stringify(body)
   $done({
     body
   })
-} else if ($request.url.indexOf('getTodayCommendAd') !== -1) {
+} else if ($request.url.indexOf('getHomePageAd')) {
   let body = JSON.stringify({})
   $done({
     body
   })
+} else if ($request.url.indexOf('getTodayCommendAd')) {
+  let body = JSON.stringify({})
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('getDataFromService')) {
+  if ($request.method === 'POST') {
+    let body = JSON.stringify({})
+    $done({
+      body
+    })
+  }
 }

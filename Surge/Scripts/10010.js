29a9bf95df3ee4e6554dd0fb5e19b4b522dd6817
lwQin <justoncesssss@gmail.com>
2020-05-08 20:45:10 +0800

    update china unicom script and module

--- Surge/Scripts/10010.js
+++ Surge/Scripts/10010.js
@@ -1,36 +1,40 @@
-console.log($request.method)
-console.log($request.url)
-console.log($response.body)
-if ($request.url.indexOf('accountListData.htm')) {
-  let body = JSON.parse($response.body)
-  delete body.adv
-  body = JSON.stringify(body)
+console.log($request.method);
+console.log($request.url);
+console.log($response.body);
+if ($request.url.indexOf("accountListData.htm") && $response.body) {
+  let body = JSON.parse($response.body);
+  delete body.adv;
+  body = JSON.stringify(body);
+  console.log(body);
   $done({
-    body
-  })
-} else if ($request.url.indexOf('indexExpressNewsAndRecommend.htm')) {
-  let body = JSON.parse($response.body)
-  body.content = []
-  body = JSON.stringify(body)
+    body,
+  });
+} else if ( $request.url.indexOf("indexExpressNewsAndRecommend.htm") && $response.body ) {
+  let body = JSON.parse($response.body);
+  body.content = [];
+  body = JSON.stringify(body);
+  console.log(body);
   $done({
-    body
-  })
-} else if ($request.url.indexOf('getHomePageAd')) {
-  let body = JSON.stringify({})
+    body,
+  });
+} else if ($request.url.indexOf("getHomePageAd") && $response.body) {
+  let body = JSON.stringify({});
+  console.log(body);
   $done({
-    body
-  })
-} else if ($request.url.indexOf('getTodayCommendAd')) {
-  let body = JSON.stringify({})
+    body,
+  });
+} else if ($request.url.indexOf("getTodayCommendAd") && $response.body) {
+  let body = JSON.stringify({});
+  console.log(body);
   $done({
-    body
-  })
-} else if ($request.url.indexOf('getDataFromService')) {
-  console.log($request.method)
-  if ($request.method === 'POST') {
-    let body = JSON.stringify({})
+    body,
+  });
+} else if ($request.url.indexOf("getDataFromService") && $response.body) {
+  if ($request.method === "POST") {
+    let body = JSON.stringify({});
+    console.log(body)
     $done({
-      body
-    })
+      body,
+    });
   }
 }

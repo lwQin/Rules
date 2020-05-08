e108be0e5d55faa9dc0f04dc7a778a67f647dc05
lwQin <justoncesssss@gmail.com>
2020-05-08 14:42:44 +0800

    update china unicom script and module

--- Surge/Scripts/10010.js
+++ Surge/Scripts/10010.js
@@ -1,31 +1,32 @@
-let body = JSON.parse($response.body)
-
-// accountListData.htm
-if ($request.url.includes('accountListData')) {
-  if (body.hasOwnProperty('adv')) {
-    delete body.adv
+if ($request.url.indexOf('accountListData.htm') !== -1) {
+  let body = JSON.parse($response.body)
+  delete body.adv
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('getHomePageAd') !== -1) {
+  let body = JSON.stringify({})
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('getDataFromService') !== -1) {
+  if ($request.method === 'POST') {
+    let body = JSON.stringify({})
+    $done({
+      body
+    })
   }
-} else if ($request.url.includes('indexExpressNewsAndRecommend')) {
-  // indexExpressNewsAndRecommend.htm
-  delete body.content
-  delete body.newsRsp
-  delete body.fiveGRsp
-} else if ($request.url.includes('messagepush')) {
-  // messagepush.htm
-  body.data = {}
-} else if ($request.url.includes('getDataFromService')) {
-  // getDataFromService.htm
-  if ($request.method === 'POST' && body.hasOwnProperty('data')) {
-    delete body.data
-  } else if ($request.method === 'GET') {
-    body = {}
-  }
-} else {
-  // getHomePageAd | getTodayCommendAd
-  body = {}
+} else if ($request.url.indexOf('indexExpressNewsAndRecommend.htm') !== -1) {
+  let body = JSON.parse($response.body)
+  body.content = []
+  body = JSON.stringify(body)
+  $done({
+    body
+  })
+} else if ($request.url.indexOf('getTodayCommendAd') !== -1) {
+  let body = JSON.stringify({})
+  $done({
+    body
+  })
 }
-
-body = (body === undefined || body === null || body === '') ? {} : JSON.stringify(body)
-$done({
-  body
-})

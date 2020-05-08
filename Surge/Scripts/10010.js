b35e911f1decc865eb2721c9607cb9673d1154ad
lwQin <justoncesssss@gmail.com>
2020-05-08 21:43:01 +0800

    update china unicom script and module

--- Surge/Scripts/10010.js
+++ Surge/Scripts/10010.js
@@ -1,49 +1,36 @@
-console.log($request.method);
-console.log($request.url);
 if ($request.url.includes("accountListData.htm") && $response.body) {
-  console.log('accountListData')
   let body = JSON.parse($response.body);
-  console.log(body);
   delete body.adv;
   body = JSON.stringify(body);
-  console.log(body);
   $done({
     body
   });
 } else if ($request.url.includes("indexExpressNewsAndRecommend.htm") && $response.body) {
-  console.log('indexExpressNewsAndRecommend')
   let body = JSON.parse($response.body);
-  console.log(body);
   body.content = [];
   body = JSON.stringify(body);
-  console.log(body);
   $done({
     body
   });
 } else if ($request.url.includes("getHomePageAd") && $response.body) {
-  console.log('getHomePageAd')
   let body = {};
   body = JSON.stringify(body);
-  console.log(body);
   $done({
     body
   });
 } else if ($request.url.includes("getTodayCommendAd") && $response.body) {
-  console.log('getTodayCommendAd')
   let body = {};
   body = JSON.stringify(body);
-  console.log(body);
   $done({
     body
   });
 } else if ($request.url.includes("getDataFromService") && $response.body) {
-  console.log('getDataFromService')
   if ($request.method === "POST") {
     let body = {};
     body = JSON.stringify(body);
-    console.log(body)
     $done({
       body
     });
   }
+  $done();
 }

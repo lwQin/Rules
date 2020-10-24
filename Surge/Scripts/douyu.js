3297a7ffa2f33e540da374bb70ca608e9798bd02
lwQin <justoncesssss@gmail.com>
2020-10-24 22:35:43 +0800

    update rules

--- Surge/Scripts/douyu.js
+++ Surge/Scripts/douyu.js
@@ -2,6 +2,8 @@ let body = JSON.parse($response.body)
 
 if ($request.url.startsWith('https://capi.douyucdn.cn/lapi/athena/room/mFollowed?')) {
   room(body)
+} else if (request.url.startsWith('https://apiv3.douyucdn.cn/mgapi/livenc/mcenter/followRoomsOnlineV2?')) {
+  removeRoomListNoLive(body)
 }
 
 body = JSON.stringify(body)
@@ -15,4 +17,14 @@ $done({
  */
 function room(body) {
   body.data = []
+}
+
+/**
+ * 移除关注页直播在放录像的
+ * @param {*} body
+ */
+function removeRoomListNoLive(body) {
+  body.data.room_list.filter((room) => {
+    return room.videoLoop === 0
+  })
 }
